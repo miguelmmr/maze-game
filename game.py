26fb28d2ebbc156d15fcab343551fdc6f5d4eda0
miguelmmr <85327891+miguelmmr@users.noreply.github.com>
2021-06-03 23:31:41 -0500

    Update game.py

--- game.py
+++ game.py
@@ -9,7 +9,7 @@ moveD = False
 moveL = False
 moveR = False
 lvlstart = True
-# WHITE = (255, 255, 255), RED = (255, 0, 0), GREEN = (0,255,0), BLUE = (0,0,255), BLACK = (0, 0, 0), B = (0,50,255)
+# WHITE = (255, 255, 255), RED = (255, 0, 0), GREEN = (0,255,0), BLUE = (0,0,255), BLACK = (0, 0, 0), B = (1,50,255)
 lvl = 1
 player = pygame.Rect(50,100,10,10)
 
